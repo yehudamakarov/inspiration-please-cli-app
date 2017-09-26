0b2c0a4d1e74b6ce592ec7e5f770ae0f959fa592
Yehuda Makarov <yehudamakarov@gmail.com>
2017-09-26 18:36:06 -0400

    About to fix issue. When scraping history, not all links will be deleted. Each pass of |e|, a can have 1 or more than 1 elements. If there is more than one link, the last element will not begin with link so it will not be deleted. see 9/4/2017

--- lib/inspiration_please/date_page.rb
+++ lib/inspiration_please/date_page.rb
@@ -3,16 +3,25 @@ class InspirationPlease::DatePage
   attr_accessor :doc, :date
 
   def initialize
-    # time = Time.new
-    # @date = time.strftime("%m/%d/%Y")
-    # @date[3] = '' if @date[3] == '0'
-    # @date[0] = '' if @date[0] == '0'
-    # date is now the end of the url for today's date_page
-
-    @doc = Nokogiri::HTML(open("http://www.chabad.org/calendar/view/day.asp?tdate=10/3/2017"))
+    time = Time.new
+    @date = time.strftime("%m/%d/%Y")
+    @date[3] = '' if @date[3] == '0'
+    @date[0] = '' if @date[0] == '0'
+    @doc = Nokogiri::HTML(open("http://www.chabad.org/calendar/view/day.asp?tdate=9/4/2017"))
   end
 
   def daily_thought
+    puts ""
+    header = doc.css("div.link.header.blue").text
+    header.delete! "\n" "\t"
+    puts "-------------#{header}-------------"
+    puts ""
+    doc.css("#DailyThoughtBody0 p").each do |e|
+      puts "#{e.text}"
+      puts ""
+    end
+    puts""
+    puts "#{doc.css("#footnotetable > div").text}"
     puts ""
   end
 
@@ -25,7 +34,8 @@ class InspirationPlease::DatePage
       doc.css("div#jewish_history.main div.link.header").each_with_index do |e, i|
         puts "-------------#{e.text}-------------"
         a = doc.css("#JewishHistoryBody#{i} > p")
-        a.pop if a[-1].text.start_with?("Links")
+        binding.pry
+        # a.pop if a[-1].text.start_with?("Link")
         puts "#{a.text}"
         puts ""
       end
