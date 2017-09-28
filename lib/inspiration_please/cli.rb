f9c085a51c8cd64cfa7e7ad11df73ab6abadfc7b
Yehuda Makarov <yehudamakarov@gmail.com>
2017-09-28 19:13:58 -0400

    Getting ready to merge to master. Working off hashes for what will be printed out.

--- lib/inspiration_please/cli.rb
+++ lib/inspiration_please/cli.rb
@@ -1,5 +1,4 @@
 class InspirationPlease::CLI
-
   attr_accessor :date_page
 
   def initialize
@@ -11,78 +10,79 @@ class InspirationPlease::CLI
     menu
   end
 
-  def list_date_options
-    puts "----- #{date_page.date} -----"
-    puts "---*-- #{date_page.date_hebrew} --*---"
-    puts "----- #{date_page.date_english} -----"
-    puts ""
-    puts "There is something special about today. "
-    puts ""
-    puts "=== - ==="
-    puts ""
-    puts ""
-  end
-
   def menu
-    puts ""
+    puts ''
     input = nil
     while input != 'exit'
-      puts "=== - ==="
-      puts ""
+      puts ''
       puts "Enter '1' for today's day in Jewish History. Enter '2' for today's Daily Thought. Or enter 'exit'."
-      puts ""
+      puts ''
       input = gets.strip
       case input
-      when "1"
+      when '1'
         print_jewish_history
-        list_date_options
-      when "2"
+      when '2'
         print_daily_thought
-        list_date_options
-      when "exit"
+      when 'exit'
         goodbye
       end
     end
   end
 
+  def list_date_options
+    puts "----- #{date_page.date} -----"
+    puts "---*-- #{date_page.date_hebrew} --*---"
+    puts "----- #{date_page.date_english} -----"
+    puts ''
+    puts 'There is something special about today. '
+    puts ''
+    puts '=== - ==='
+    puts ''
+    puts ''
+  end
+
   def print_jewish_history
     if date_page.jewish_history?
       date_page.history.each do |header, content|
+        puts ''
+        puts "    - #{date_page.date_hebrew} -    "
+        puts ''
         puts "-------------#{header}-------------"
-        puts ""
-        puts "#{content}"
-        puts ""
-        puts "--==--"
+        puts ''
+        puts content.to_s
+        puts ''
+        puts '--==--'
       end
     else
-      puts ""
-      puts "Today in Jewish History is a canvas waiting to be painted by you. Try the daily thought. :) "
-      puts ""
+      puts ''
+      puts 'Today in Jewish History is a canvas waiting to be painted by you. Try the daily thought. :) '
+      puts ''
     end
   end
 
-  def date
-  end
-
+  def date; end
 
   def print_daily_thought
     if date_page.daily_thought?
       date_page.daily_thought.each do |header, content|
+        puts ''
+        puts "    - #{date_page.date_hebrew} -    "
+        puts ''
         puts "-------------#{header}-------------"
-        puts ""
-        puts "#{content}"
-        puts ""
+        puts ''
+        puts content.to_s
+        puts ''
       end
     else
-      puts ""
-      puts "Maybe you have a daily thought? :) "
-      puts ""
+      puts ''
+      puts 'Maybe you have a daily thought? :) '
+      puts ''
     end
   end
 
   def goodbye
-    puts""
-    puts "Make it a day full of good things."
-    puts ""
+    puts ''
+    puts 'Make it a day full of good things.'
+    puts ''
   end
 end
--- lib/inspiration_please/date_page.rb
+++ lib/inspiration_please/date_page.rb
@@ -1,14 +1,12 @@
 class InspirationPlease::DatePage
+  # Must print Hebrew and English date.
 
-# Must print Hebrew and English date.
-
-
-# should also build in "curious about a specific date?" enter and pull that down.
+  # should also build in "curious about a specific date?" enter and pull that down.
 
   attr_accessor :doc, :date, :history, :daily_thought, :date_english, :date_hebrew
   def initialize
     time = Time.new
-    @date = time.strftime("%m/%d/%Y")
+    @date = time.strftime('%m/%d/%Y')
     @date[3] = '' if @date[3] == '0'
     @date[0] = '' if @date[0] == '0'
 
@@ -18,25 +16,23 @@ class InspirationPlease::DatePage
     jewish_history_hash
     @daily_thought = {}
     daily_thought_hash
-    @date_english = doc.search("#PrimaryHeader").text
-    @date_hebrew = doc.search("#SecondaryHeader > a").text
-    binding.pry
-
+    @date_english = doc.search('#PrimaryHeader').text
+    @date_hebrew = doc.search('#SecondaryHeader > a').text
   end
 
-
-
   def jewish_history_hash
-    doc.css("div#jewish_history.main div.link.header").each_with_index do |e, i|
-      @history["#{e.text}"] = "#{(doc.css("#JewishHistoryBody#{i} > p").map{|d| "" << "#{d.text}" unless d.text.include?("Link")}.take 1).join}"
+    doc.css('div#jewish_history.main div.link.header').each_with_index do |e, i|
+      @history[e.text.to_s] = (doc.css("#JewishHistoryBody#{i} > p").map do
+        |d| '' << d.text.to_s unless d.text.include?('Link')
+      end.take 1).join.to_s
     end
   end
 
   def daily_thought_hash
-    text = ""
-    doc.css("#DailyThoughtBody0 p").each{|e| text << "#{e.text} \n\n"}
-    text << "#{doc.css("#footnotetable > div").text}"
-    @daily_thought[doc.css("div.link.header.blue").text.delete! "\n" "\t"] = text.strip
+    text = ''
+    doc.css('#DailyThoughtBody0 p').each { |e| text << "#{e.text} \n\n" }
+    text << doc.css('#footnotetable > div').text.to_s
+    @daily_thought[doc.css('div.link.header.blue').text.delete! "\n" "\t"] = text.strip
   end
 
   def jewish_history?
@@ -46,5 +42,4 @@ class InspirationPlease::DatePage
   def daily_thought?
     !daily_thought.empty?
   end
-
 end
